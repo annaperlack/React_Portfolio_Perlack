8a4e9ab9dc457a763c48905855eba2a5100044b9
Anna Perlack <annaperlack@gmail.com>
2023-04-17 16:43:18 -0400

    added project 3-quizzinator

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import lazyDater from '../images/screenshot-lazy-dater.png';
 import cartoonify from '../images/profilepage.png'
+import quizzinator from '../images/quizzinator.png'
+
 
 export default function Portfolio() {
     return (
@@ -22,6 +24,14 @@ export default function Portfolio() {
                         <h5 class="card-title"><a href="https://github.com/FDJoss/The-Lazy-Dater">The Lazy Dater</a></h5>
                         <p class="card-text">This website is built with HTML, CSS, and Javascript. It utilizes two APIs, the Cocktail Database and the Movie Database, to pull in cocktail and movie information.</p>
                         <a href="https://fdjoss.github.io/The-Lazy-Dater/" class="btn btn-primary">Try it out!</a>
+                    </div>
+                </div>
+                <div class="card col-3 m-3">
+                    <img src={quizzinator} class="card-img-top" alt="cartoonify" />
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="https://github.com/annaperlack/cartoonify-my-pic">Cartoonify My Pic</a></h5>
+                        <p class="card-text"></p>
+                        <a href="https://quizzinator.herokuapp.com/" class="btn btn-primary">Try it out!</a>
                     </div>
                     
                 </div>
